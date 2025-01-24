1a7d8457956c89aba9a1eea5451e4ec7f5c1f8f6
tomttk <tom.tk@hotmail.fr>
2025-01-24 15:47:09 +0100

    circleshape & player classes

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
-    
-    pygame.init() 
-
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        pygame.Surface.fill(screen, (0, 0, 0))
 
-
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
